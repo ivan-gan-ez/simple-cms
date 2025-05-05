182360c81c8fca9d9d0af811db3f47148496c716
Ivan Gan <ivan.ee.zern.gan@gmail.com>
2025-05-05 15:02:00 +0800

    made posts a working thing woooo

--- pages/home.php
+++ pages/home.php
@@ -12,49 +12,43 @@
     $query->execute();
 
     // 3: eat (fetch all results from the query)
-    $tasks = $query->fetchAll();
+    $users = $query->fetchAll();
+
+    // Get data from database again
+    // 3.25: recipe (sql command)
+    $sql = "SELECT * FROM posts";
+
+    // 3.5: prepare material (prepare sql query)
+    $query = $database->prepare($sql);
+
+    // 3.75: cook it (execute the sql query)
+    $query->execute();
+
+    // 4: eat (fetch all results from the query)
+    $posts = $query->fetchAll();
 ?>
 
 <?php require "parts/header.php"?>
     
-    <div class="container mx-auto my-5" style="max-width: 500px;">
-      <h1 class="h1 mb-4 text-center">My Blog</h1>
+<div class="container mx-auto my-5" style="max-width: 500px;">
+  <h1 class="h1 mb-4 text-center">My Blog</h1>
+
+  <?php foreach ($posts as $i => $post) {?>
       <div class="card mb-2">
         <div class="card-body">
-          <h5 class="card-title">Post 4</h5>
-          <p class="card-text">Here's some content about post 4</p>
+          <h5 class="card-title"><?=$posts[$i]['title']?></h5>
+          <p class="card-text">by <?=$posts[$i]['user']?></p>
+          <p class="card-text"><?=$posts[$i]['shortdesc']?></p>
           <div class="text-end">
-            <a href="/post" class="btn btn-primary btn-sm">Read More</a>
+            <form method="POST" action="/post">
+              <input type="hidden" name="post_id" value="<?= $post["id"];?>" />
+              <button class="btn btn-primary btn-sm">Read More</button>
+            </form>
           </div>
         </div>
       </div>
-      <div class="card mb-2">
-        <div class="card-body">
-          <h5 class="card-title">Post 3</h5>
-          <p class="card-text">This is for post 3</p>
-          <div class="text-end">
-            <a href="/post" class="btn btn-primary btn-sm">Read More</a>
-          </div>
-        </div>
-      </div>
-      <div class="card mb-2">
-        <div class="card-body">
-          <h5 class="card-title">Post 2</h5>
-          <p class="card-text">This is about post 2</p>
-          <div class="text-end">
-            <a href="/post" class="btn btn-primary btn-sm">Read More</a>
-          </div>
-        </div>
-      </div>
-      <div class="card mb-2">
-        <div class="card-body">
-          <h5 class="card-title">Post 1</h5>
-          <p class="card-text">This is a post</p>
-          <div class="text-end">
-            <a href="/post" class="btn btn-primary btn-sm">Read More</a>
-          </div>
-        </div>
-      </div>
+  <?php } ?>
+
 
       <div class="mt-4 d-flex justify-content-center gap-3">
         <?php if ( isset($_SESSION['user']['name']) ) {?>
